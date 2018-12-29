5b5ca68858feb6077127965009ccb0f40df9be8e
José Troncoso Molina <>
2018-12-28 21:30:04 -0300

    Agregando archivo de mi primera pagina web

---
+++ Tarea 5 en R.R
@@ -0,0 +1,61 @@
+<html>
+  <head>
+  <style>
+  .Blanco{background-color: white;}
+.teal {background-color: teal;}
+body{border:solid 1px black;}
+div{float:left;}
+table,tr,th,td{border:solid 1px black;}
+</style>
+  </head>
+  <title></title>
+  </head>
+  <body class="white">
+  <div id="Principal">
+  <header>
+  <div> 
+  
+  <div>
+  <div>
+  </div>
+  <div>
+  </div>	
+  </div>
+  <h2>Deportes Melipilla contrata a Ronaldinho</h2>	
+  <img src="Ronaldinho.jpg" border="1" alt="Deportes Melipilla contrata a Ronaldinho" width="400" height="300">
+  <div>
+  
+  <h2>Por 5 millones de dolares el ex Barcelona llega al futbol nacional</h2>
+  <p>El club del potro solitario fich&oacute; recientemente al astro brasilero, que llega al club por una cifra cercana a 5 millones de dolares. Roni se mostr&oacute; feliz ante las camaras de formar parte del club del maipo para la proxima temporada, donde reconoci&oacute; que desde joven tuvo el objetivo de jugar por Deportes Melipilla. "Cuando conoc&iacute; el Roberto Bravo quede asombrado, el estadio es muy bonito" dijo el ex campeon del mundo en su entrevista a un diario local<p>
+  <p>Adem&aacute;s, el presidente del club  anunci&oacute;: "Queremos ganar la copa libertadores, estamos en contacto con otros jugadores"<p>
+  <div>
+  <table style="width: 100%">	
+  <tr class="teal">
+  <th>Deportes Melipilla</th>
+  <th>Sueldo Mensual Neto</th>
+  <th>Cotizaciones Previsionales</th>
+  <th>Costo de traspaso</th>
+  <th>Clausula de salida</th>
+  
+  </tr>
+  <tr class="teal">
+  <td>Fernando Meneses</td>
+  <td>$5.000.000</td>
+  <td>$130.000</td>
+  <td>$465.000.000</td>
+  <td>$89.000.000</td>
+  
+  </tr>
+  <tr class="teal">
+  <td>Jose Luis Cabi&oacuten;</td>
+  <td>$3.500.000</td>
+  <td>$82.000</td>
+  <td>$389.000.000</td>
+  <td>$29.000.000</td>
+  
+  </tr>	
+  </div>	
+  </div>
+  </body>
+  </html>
+  
